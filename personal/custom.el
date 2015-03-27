dffdb8db44258e3b6287ba9b62705a416a035334
tdaole <tdaole@quantcast.com>
2015-03-28 00:22:52 +0800

    ECB

--- personal/custom.el
+++ personal/custom.el
@@ -3,7 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ecb-layout-window-sizes
+   (quote
+    (("left8"
+      (ecb-directories-buffer-name 0.2379182156133829 . 0.2835820895522388)
+      (ecb-sources-buffer-name 0.2379182156133829 . 0.23880597014925373)
+      (ecb-methods-buffer-name 0.2379182156133829 . 0.29850746268656714)
+      (ecb-history-buffer-name 0.2379182156133829 . 0.16417910447761194)))))
  '(ecb-options-version "2.40")
+ '(ecb-primary-secondary-mouse-buttons (quote mouse-1--C-mouse-1))
  '(recentf-max-menu-items 25)
  '(recentf-max-saved-items 25)
  '(xterm-mouse-mode t))
@@ -20,9 +28,8 @@
  '(font-lock-string-face ((((class color) (min-colors 88) (background light)) (:foreground "color-160"))))
  '(font-lock-type-face ((((class color) (min-colors 88) (background light)) (:foreground "color-33" :weight ultra-bold))))
  '(font-lock-variable-name-face ((((class color) (min-colors 88) (background light)) (:foreground "color-166"))))
- '(linum ((t (:inherit (shadow default) :foreground "color-248"))))
- '(minibuffer-prompt ((t (:foreground "color-118"))))
+ '(linum ((t (:inherit (shadow default) :background "#3F3F3F" :foreground "gray62"))))
+ '(minibuffer-prompt ((t (:foreground "Green"))))
  '(semantic-highlight-edits-face ((((class color) (background light)) (:background "color-165"))))
- '(semantic-idle-local-symbol-highlight-mode ((((class color) (background light)) (:background "color-57"))))
- '(which-func ((((class color) (min-colors 88) (background light)) (:foreground "color-57" :weight ultra-bold))))
- )
+ '(semantic-idle-symbol-highlight ((t (:inherit region :background "royal blue"))))
+ '(which-func ((((class color) (min-colors 88) (background light)) (:foreground "color-57" :weight ultra-bold)))))
