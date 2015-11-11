d9f62c8b492e90f19792ac2865f02c67e5ab372c
Trong Dao Le <tdaole@quantcast.com>
2015-11-10 16:33:53 -0800

    Upgrade

--- personal/custom.el
+++ personal/custom.el
@@ -12,6 +12,9 @@
       (ecb-history-buffer-name 0.2379182156133829 . 0.16417910447761194)))))
  '(ecb-options-version "2.40")
  '(ecb-primary-secondary-mouse-buttons (quote mouse-1--C-mouse-1))
+ '(package-selected-packages
+   (quote
+    (zop-to-char zenburn-theme yari yaml-mode volatile-highlights vkill undo-tree sr-speedbar smex smartrep smartparens smart-mode-line scala-mode2 ruby-tools rainbow-mode rainbow-delimiters puppet-mode protobuf-mode php-mode ov operate-on-number move-text markdown-mode magit json-mode js2-mode inf-ruby ido-ubiquitous helm-projectile helm-gtags helm-descbinds helm-ag haskell-mode guru-mode grizzl gotest google-c-style god-mode go-projectile gitignore-mode gitconfig-mode git-timemachine gist ggtags geiser function-args flycheck flx-ido expand-region exec-path-from-shell elisp-slime-nav ecb easy-kill discover-my-major diminish diff-hl d-mode csv-mode company-go company-anaconda color-theme clojure-test-mode cdlatex browse-kill-ring anzu ack-and-a-half ace-window ace-jump-buffer)))
  '(recentf-max-menu-items 25)
  '(recentf-max-saved-items 25)
  '(xterm-mouse-mode t))
